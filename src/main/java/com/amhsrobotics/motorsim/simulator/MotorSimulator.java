4df906c39ac26b44ca1d7a03c4dff41eae77f2e1
Owen Leather <owenleather22@mittymonarch.com>
2019-11-03 18:14:11 -0800

    updated javadoc and added jitpack

--- src/main/java/com/amhsrobotics/motorsim/simulator/MotorSimulator.java
+++ src/main/java/com/amhsrobotics/motorsim/simulator/MotorSimulator.java
@@ -7,8 +7,7 @@ import com.amhsrobotics.motorsim.motors.Motor;
 /**
  * Simulates a mechanism given data on the motors that drive it.
  * <p>
- * References:
- * The voltage to acceleration calculations are from 971's series about control system modeling:
+ * The motor modeling calculations are from 971's series about control system modeling and test driven development:
  * https://www.youtube.com/watch?v=uGtT8ojgSzg
  * https://www.youtube.com/watch?v=RLrZzSpHP4E
  */
@@ -25,18 +24,41 @@ public class MotorSimulator {
 	private double Kt; //torque constant
 	private double resistance;
 	
-	
-
-	
 	private ControlLoop controlLoop;
 	private ControlType controlType;
 	private String name;
 	
-	
+	/**
+	 * Motor Simulator Constructor
+	 *
+	 * Initiates the properties of the motor simulator.
+	 *
+	 * @param motor
+	 * @param numMotors
+	 * @param mass
+	 * @param gearRatio
+	 * @param wheelRadius
+	 * @param controlLoop
+	 * @param controlType
+	 */
 	public MotorSimulator(Motor motor, double numMotors, double mass, double gearRatio, double wheelRadius, ControlLoop controlLoop, ControlType controlType) {
 		this(motor,numMotors,mass,gearRatio,wheelRadius,controlLoop,controlType,  "Motor Sim");
 	}
 	
+	/**
+	 * Motor Simulator Constructor
+	 *
+	 * Initiates the properties of the motor simulator.
+	 *
+	 * @param motor
+	 * @param numMotors
+	 * @param mass
+	 * @param gearRatio
+	 * @param wheelRadius
+	 * @param controlLoop
+	 * @param controlType
+	 * @param name
+	 */
 	public MotorSimulator(Motor motor, double numMotors, double mass, double gearRatio, double wheelRadius, ControlLoop controlLoop, ControlType controlType, String name) {
 		this.motor = motor;
 		this.numMotors = numMotors;
@@ -56,11 +78,17 @@ public class MotorSimulator {
 		this.Kt = (numMotors * stallTorque) / stallCurrent;
 	}
 	
-	
 	private double velocity = 0;
 	private double position = 0;
 	private double voltage = 0;
 	
+	/**
+	 * Iterates through the motor model and graphs it over time
+	 *
+	 * @param setpoint
+	 * @param simulationTime
+	 * @param iterationTime
+	 */
 	public void graphModel(double setpoint, double simulationTime, double iterationTime){
 		Graph graph = new Graph(name);
 		
@@ -78,6 +106,13 @@ public class MotorSimulator {
 		}
 	}
 	
+	/**
+	 * Updates the motor model.
+	 *
+	 * Calculates the voltage from a {@link ControlLoop} and gets the applies the acceleration calculation to it.
+	 * @param setpoint
+	 * @param iterationTime
+	 */
 	public void update(double setpoint, double iterationTime){
 		if(controlType == ControlType.POSITION){
 			voltage = controlLoop.update(setpoint,position);
@@ -95,6 +130,16 @@ public class MotorSimulator {
 		velocity += acceleration * iterationTime;
 	}
 	
+	/**
+	 * Calculates acceleration from a given voltage following the input motor model.
+	 *
+	 * @param voltage
+	 * @return
+	 */
+	private double getAcceleration(double voltage) {
+		return -Kt * gearRatio * gearRatio / (Kv * resistance * wheelRadius * wheelRadius * mass) * velocity + gearRatio * Kt / (resistance * wheelRadius * mass) * voltage;
+	}
+	
 	public double getVelocity(){
 		return velocity;
 	}
@@ -107,7 +152,5 @@ public class MotorSimulator {
 		return voltage;
 	}
 	
-	private double getAcceleration(double voltage) {
-		return -Kt * gearRatio * gearRatio / (Kv * resistance * wheelRadius * wheelRadius * mass) * velocity + gearRatio * Kt / (resistance * wheelRadius * mass) * voltage;
-	}
+
 }
